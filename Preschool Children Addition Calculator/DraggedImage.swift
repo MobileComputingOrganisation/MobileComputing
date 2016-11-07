66860783d675fb411b16937809c507b4cfaf9097
Mario Zammit <mariozammit@Marios-MacBook-Pro.local>
2016-11-07 10:15:33 -0800

    Fixed the constraint of the apple images where they can be dragged and made the app available only in portrait only.

--- Preschool Children Addition Calculator/DraggedImage.swift
+++ Preschool Children Addition Calculator/DraggedImage.swift
@@ -19,22 +19,23 @@ class DraggedImage: UIImageView {
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         
+        //calculate the offset
         let currentLocation = touches.first?.location(in: self)
         let dx = currentLocation!.x - startLocation!.x
         let dy = currentLocation!.y - startLocation!.y
-        
         self.center = CGPoint(x: self.center.x+dx, y: self.center.y+dy)
-    }
-    
-    //when user stops touching the element
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         
-        /* 
-         * if apples are on the grey background return to start location because
-         * touch does not work on apples in the grey bacground view
-         */
-        if (center.y < 0 ){
-            self.center = startLocation!
-        }
+        // Constrain movement into parent bounds
+        let midx = self.bounds.midX;
+        
+        center.x = max(midx, center.x)
+        center.x = min((self.superview?.bounds.size.width)! - midx, center.x)
+        
+        let midy = self.bounds.midY;
+        center.y = max(midy, center.y)
+        center.y = min((self.superview?.bounds.size.height)! - midy, center.y)
+        
+        // Set new location
+        self.center = center
     }
 }
